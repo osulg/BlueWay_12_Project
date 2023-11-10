c481bb65bfa428661b11461a3849ce200cb96d71
230759264 <thdwldn1204@naver.com>
2023-11-10 23:34:42 +0900

    버튼 동적 생성 & / api 1호선만 선별 & 보이기 / 버튼 연결

--- app/src/main/java/com/example/activity/ApiSubway1.kt
+++ app/src/main/java/com/example/activity/ApiSubway1.kt
@@ -8,9 +8,9 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
 
-fun main(stationId:String): String? {
+fun main(stationId:String,num:Int): String? {
 
-    var station=stationId
+    val station=stationId
     val urlBuilder = StringBuilder("http://swopenAPI.seoul.go.kr/api/subway/") /*URL*/
     urlBuilder.append(
         "/" + URLEncoder.encode(
@@ -28,7 +28,70 @@ fun main(stationId:String): String? {
     urlBuilder.append("/" + URLEncoder.encode("0", "UTF-8")) /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
     urlBuilder.append(
         "/" + URLEncoder.encode(
-            "10",
+            "50",
+            "UTF-8"
+        )
+    ) /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
+    // 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다..
+
+    // 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
+    urlBuilder.append("/" + URLEncoder.encode("${station}", "UTF-8")) /* 서비스별 추가 요청인자들 */
+    val url = URL(urlBuilder.toString())
+    val conn = url.openConnection() as HttpURLConnection
+    conn.requestMethod = "GET"
+    conn.setRequestProperty("Content-type", "application/xml")
+    println("Response code: " + conn.responseCode) /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
+    val rd: BufferedReader
+
+    // 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
+    rd = if (conn.responseCode >= 200 && conn.responseCode <= 300) {
+        BufferedReader(InputStreamReader(conn.inputStream))
+    } else {
+        BufferedReader(InputStreamReader(conn.errorStream))
+    }
+    val sb = StringBuilder()
+    var line: String?
+    while (rd.readLine().also { line = it } != null) {
+        sb.append(line)
+    }
+    rd.close()
+    conn.disconnect()
+
+    val parser = JsonParser()
+    val jsonObject = parser.parse(sb.toString()).asJsonObject
+    val trainLineNm = jsonObject.get("realtimeArrivalList").asJsonArray[num-1].asJsonObject.get("trainLineNm").asString
+    val s = " | 현재 역 : "
+    val arvlMsg2=jsonObject.get("realtimeArrivalList").asJsonArray[num-1].asJsonObject.get("arvlMsg2").asString
+    val specificData1=StringBuilder(trainLineNm).append(s).append(arvlMsg2).toString()
+    return specificData1
+
+}
+
+fun return_num(stationId:String): Pair<Int, Array<Int>> {
+
+    var numbers20:Array<Int>
+    numbers20=arrayOf(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+    var i = 0
+
+    val station=stationId
+    val urlBuilder = StringBuilder("http://swopenAPI.seoul.go.kr/api/subway/") /*URL*/
+    urlBuilder.append(
+        "/" + URLEncoder.encode(
+            "566945517364687439357769727444",
+            "UTF-8"
+        )
+    ) /*인증키 (sample사용시에는 호출시 제한됩니다.)*/
+    urlBuilder.append("/" + URLEncoder.encode("json", "UTF-8")) /*요청파일타입 (xml,xmlf,xls,json) */
+    urlBuilder.append(
+        "/" + URLEncoder.encode(
+            "realtimeStationArrival",
+            "UTF-8"
+        )
+    ) /*서비스명 (대소문자 구분 필수입니다.)*/
+    urlBuilder.append("/" + URLEncoder.encode("0", "UTF-8")) /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
+    urlBuilder.append(
+        "/" + URLEncoder.encode(
+            "50",
             "UTF-8"
         )
     ) /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
@@ -59,7 +122,22 @@ fun main(stationId:String): String? {
 
     val parser = JsonParser()
     val jsonObject = parser.parse(sb.toString()).asJsonObject
-    val specificData1 = jsonObject.get("realtimeArrivalList").asJsonArray[0].asJsonObject.get("trainLineNm").asString
-    return specificData1
+    var totalCount = jsonObject.get("realtimeArrivalList").asJsonArray[0].asJsonObject.get("totalCount").asInt
+    for (num in 1..totalCount!!)
+    {
+        val subwayId=jsonObject.get("realtimeArrivalList").asJsonArray[num-1].asJsonObject.get("subwayId").asInt
+        if (subwayId!=1001)
+        {
+            totalCount=totalCount-1
+        }
+        if (subwayId==1001)
+        {
+            numbers20[i]=num
+            i++
+        }
+
+    }
+
+    return Pair(totalCount,numbers20)
 
 }
--- app/src/main/java/com/example/activity/Sub1Activity.kt
+++ app/src/main/java/com/example/activity/Sub1Activity.kt
@@ -2,10 +2,14 @@ package com.example.activity
 
 import Fragment.alarm
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.core.view.ViewCompat
 import com.example.activity.databinding.ActivitySub11Binding
 import com.example.activity.databinding.ActivitySub1Binding
 import kotlin.concurrent.thread
@@ -13,43 +17,64 @@ import kotlin.concurrent.thread
 
 class Sub1Activity : AppCompatActivity() {
 
-    private var mBinding:ActivitySub1Binding?=null
-    private val binding get()=mBinding!!
+    private var mBinding: ActivitySub1Binding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding=ActivitySub1Binding.inflate(layoutInflater)
+        mBinding = ActivitySub1Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
         //search
-        val btn_search:Button=findViewById(R.id.imageViewSearchButton)
+        val btn_search: Button = findViewById(R.id.imageViewSearchButton)
+        val buttonView = findViewById<LinearLayout>(R.id.buttonView)
+        btn_search.setOnClickListener {
+            val stationId = binding.searchStation.text.toString()
 
-        btn_search.setOnClickListener{
-            val stationId=binding.searchStation.text.toString()
             thread {
-                var resultId=main(stationId)
-                runOnUiThread{ Toast.makeText(this@Sub1Activity, resultId, Toast.LENGTH_LONG).show()}
+                val (totalCount,array20) = return_num(stationId)
+                for (num in 1..totalCount!!) {
+                    val stationButton = Button(this)
+                    val layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                    thread {
+                        val resultId = main(stationId, array20[num-1])
+                        stationButton.layoutParams = layoutParams
+                        stationButton.text = resultId
+                        stationButton.id = ViewCompat.generateViewId()
+                        runOnUiThread {
+                            buttonView.addView(stationButton)
+                        }
+                    }
+                    stationButton.setOnClickListener {
+                        val intent = Intent(this, Sub11Activity::class.java)
+                        startActivity(intent)
+                    }
+                }
+
             }
-        }
 
-        // fragment
-        val fragment1 = alarm()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.sub1frame, fragment1)
-            .addToBackStack(null)
-            .commit()
+            // fragment
+            val fragment1 = alarm()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.sub1frame, fragment1)
+                .addToBackStack(null)
+                .commit()
 
-        //Main Back
-        val btn_bmain: Button =findViewById(R.id.imageViewArrow)
-        btn_bmain.setOnClickListener {
-            finish()
-        }
+            //생성된 버튼 삭제 (새로고침)
+            val refreshView: Button = findViewById(R.id.ButtonRefresh)
+            refreshView.setOnClickListener {
+                buttonView.removeAllViews()
+            }
 
-        //sub11 Go
-        val btn_sub11:Button=findViewById(R.id.btn_sub11)
-        btn_sub11.setOnClickListener {
-            val intent = Intent(this,Sub11Activity::class.java)
-            startActivity(intent)
+            //Main Back
+            val btn_bmain: Button = findViewById(R.id.imageViewArrow)
+            btn_bmain.setOnClickListener {
+                finish()
+            }
+
         }
     }
 }
